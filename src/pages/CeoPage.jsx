a605262fa4ace3e39831f79672c327954698eb18
k-risu <kkd4869@gmail.com>
2024-11-25 12:14:05 +0900

    [jsx]기초

---
+++ src/pages/CeoPage.jsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header";
+
+function CeoPage() {
+  return (
+    <>
+      <Header></Header>
+      <main>대표인사말</main>
+      <footer>하단</footer>
+    </>
+  );
+}
+
+export default CeoPage;
