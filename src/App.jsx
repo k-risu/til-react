dcf12e37d09c847a7ef0bc6356bb4b7c0b76d9b1
k-risu <kkd4869@gmail.com>
2024-12-24 12:59:18 +0900

    [docs] useRef 정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,107 +1,33 @@
-import { useContext } from "react";
-import { UserInfoContext, UserInfoProvider } from "./contexts/UserInfoContext";
-
-const Header = () => {
-  const { userInfo, setUserInfo } = useContext(UserInfoContext);
-  return (
-    <header>
-      <div style={{ display: "flex", justifyContent: "space-between" }}>
-        <p>로고</p>
-        <nav>
-          {userInfo.userId === "" ? (
-            <div>
-              <button
-                onClick={() => {
-                  setUserInfo({
-                    userId: "hong",
-                    userName: "길동",
-                    userRole: "MEMBER",
-                  });
-                }}
-              >
-                로그인
-              </button>
-              <button onClick={() => {}}>회원가입</button>
-            </div>
-          ) : (
-            <div>
-              <button
-                onClick={() => {
-                  setUserInfo({ userId: "", userName: "", userRole: "GUEST" });
-                }}
-              >
-                로그아웃
-              </button>
-              <button onClick={() => {}}>{userInfo.userName}님 정보수정</button>
-            </div>
-          )}
-        </nav>
-      </div>
-    </header>
-  );
-};
-const Footer = () => {
-  const { userInfo } = useContext(UserInfoContext);
-  return <footer>하단 {userInfo.userRole}</footer>;
-};
-const Main = () => {
-  const { userInfo } = useContext(UserInfoContext);
-  return (
-    <main>
-      {userInfo.userId === "" ? (
-        <div>로그인을 하셔야 서비스를 이용합니다.</div>
-      ) : (
-        <div>
-          <Chracter />
-          <Friend />
-          <Point />
-          <Map />
-          <FAQ />
-        </div>
-      )}
-    </main>
-  );
-};
-
-const Chracter = () => {
-  const { userInfo } = useContext(UserInfoContext);
-  return (
-    <div>
-      <div>{userInfo.userName}님 캐릭터 변경 서비스</div>
-      <ChoiceCharacter>캐릭터 종류 선택 서비스</ChoiceCharacter>
-    </div>
-  );
-};
-const ChoiceCharacter = () => {
-  const { userInfo } = useContext(UserInfoContext);
-  return <div>{userInfo.userName}님 캐릭터 종류 선택 서비스</div>;
-};
-
-const Friend = () => {
-  const { userInfo } = useContext(UserInfoContext);
-  return <div>{userInfo.userName}님 친구관리 서비스</div>;
-};
-const Point = () => {
-  const { userInfo } = useContext(UserInfoContext);
-  return <div>{userInfo.userName}님 포인트 구매 서비스</div>;
-};
-const Map = () => {
-  const { userInfo } = useContext(UserInfoContext);
-  return <div>{userInfo.userName}님 주변 서비스 지도안내 서비스</div>;
-};
-const FAQ = () => {
-  const { userInfo } = useContext(UserInfoContext);
-  return <div>{userInfo.userName}님 고객센터 QA 서비스</div>;
-};
+import { useRef } from "react";
 
 function App() {
+  const videoRef = useRef(null);
+  const prevV = () => {
+    videoRef.current.currentTime -= 10;
+    videoRef.current.play();
+  };
+  const playV = () => {
+    videoRef.current.currentTime = 0;
+    videoRef.current.play();
+  };
+  const stopV = () => {
+    videoRef.current.currentTime = 0;
+    videoRef.current.pause();
+  };
+  const nextV = () => {
+    videoRef.current.currentTime += 10;
+    videoRef.current.play();
+  };
   return (
     <div>
-      <UserInfoProvider>
-        <Header />
-        <Main />
-        <Footer />
-      </UserInfoProvider>
+      <h1>video 제어</h1>
+      <video ref={videoRef} src="비디오주소url" muted autoPlay controls></video>
+      <div>
+        <button onClick={prevV}>10초전</button>
+        <button onClick={playV}>play</button>
+        <button onClick={stopV}>stop</button>
+        <button onClick={nextV}>10초후</button>
+      </div>
     </div>
   );
 }
