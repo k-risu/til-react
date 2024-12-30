45750abb6e6d4a9f5ba4ab161153453dd0ec12bc
k-risu <kkd4869@gmail.com>
2024-12-30 13:07:38 +0900

    [docs] useReducer 정리

---
+++ src/store/reducers/countReducer.js
@@ -0,0 +1,13 @@
+// Reducer 함수
+export function counteReducer(state, action) {
+  switch (action.type) {
+    case "add":
+      return { count: state.count + 1 };
+    case "minus":
+      return { count: state.count - 1 };
+    case "reset":
+      return { count: 0 };
+    default:
+      return state;
+  }
+}
